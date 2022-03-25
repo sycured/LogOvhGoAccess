67bce5884dd3cc3bae4366f61860f5fdd0cd87d8
Cañadas Pascal <pascal@mirandais.fr>
2022-03-25 16:19:45 +0100

    First Commit

---
+++ script_daily.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#Usage:         ./script_daily.sh domain
+#ex:            ./script_daily.sh ndd.fr
+
+
+#Date de la veille
+DATE=$(date --date=yesterday "+%d-%m-%Y")
+#Mois
+MONTH=$(date --date=yesterday "+%m-%Y")
+#nom du domaine
+#ex: ndd.fr
+SITE=$1
+#Login compte OVH
+LOGIN=utilisateur # voir pour le recupérer via des variables environnement
+#Mot de passe compte OVH
+PASSWORD=password # voir pour le recupérer via des variables environnement
+#Dossier de stockage
+DOSSIER=$SITE/logs/$MONTH/
+DOSSIERGOACCESS=$SITE/goaccess/$MONTH/
+#Numéro du cluster ovh https://logs.XXXXXXX.hosting.ovh.net/
+CLUSTER=cluster005
+#Creation de url pour récupérer les statistiques
+LOGS='https://logs.'$CLUSTER'.hosting.ovh.net/'$SITE'/logs'
+#Creation du dossier LOG
+mkdir -p $DOSSIER
+mkdir -p $DOSSIERGOACCESS
+# Téléchargement du log
+CURRENTLOG=$DOSSIER$SITE-$DATE.log.gz
+wget -nv -nd -R A.gz -P$DOSSIER $LOGS/logs-$MONTH/$SITE-$DATE.log.gz --http-user=$LOGIN --http-password=$PASSWORD -o $DOSSIER/logs-wget.txt
+# Traitement dans goaccess
+zcat -f $CURRENTLOG | goaccess --log-format=COMBINED -o $DOSSIERGOACCESS$DATE.html
+rm -rf $DOSSIER
---
+++ script_imediate.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#Usage:         ./script_imediate.sh domain
+#ex:            ./script_imediate.sh ndd.fr
+
+#nom du domaine
+#ex: ndd.fr
+SITE=$1
+DATE=$(date "+%d-%m-%Y")
+#Login compte OVH
+LOGIN=utilisateur # voir pour le recupérer via des variables environnement
+#Mot de passe compte OVH
+PASSWORD=password # voir pour le recupérer via des variables environnement
+#Dossier de stockage
+DOSSIER=$SITE/logs/today/
+DOSSIERGOACCESS=$SITE/goaccess/today/
+FILENAME=index
+#Numéro du cluster ovh https://logs.XXXXXXX.hosting.ovh.net/
+CLUSTER=cluster005
+#Creation de url pour récupérer les statistiques
+LOGS='https://logs.'$CLUSTER'.hosting.ovh.net/'$SITE'/osl'
+#Creation du dossier LOG
+mkdir -p $DOSSIER
+mkdir -p $DOSSIERGOACCESS
+# Téléchargement du log
+CURRENTLOG=$DOSSIER$SITE-$DATE.log
+wget -nv -nd -R A.gz -P$DOSSIER $LOGS/$SITE-$DATE.log --http-user=$LOGIN --http-password=$PASSWORD -o $DOSSIER/logs-wget.txt
+# Traitement dans goaccess
+goaccess $CURRENTLOG --log-format=COMBINED -o $DOSSIERGOACCESS$FILENAME.html
+rm -rf $DOSSIER
